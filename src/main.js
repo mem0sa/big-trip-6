0df33de3d26882972334246b49c6b5f98372710d
mem0sa <maksim.r.2007@mail.ru>
2026-03-12 19:37:05 +0500

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import PointsModel from './model/points-model.js';
 import DestinationModel from './model/destination-model.js';
 import OffersModel from './model/offers-model.js';
 
-
 const filterContainer = document.querySelector('.trip-controls__filters');
 const tripContainer = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
